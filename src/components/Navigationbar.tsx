477f7a560ff86a287f700f8823020f7f1e87afc4
taiyou <hiromacha1116@icloud.com>
2023-09-24 11:36:10 +0900

    NavigationUIを変更

--- src/components/Navigationbar.tsx
+++ src/components/Navigationbar.tsx
@@ -9,18 +9,19 @@ export default function NavigationbarComponent(props: NavigationProps) {
   const { categoryName, setCategoryName, onGetRecipeData } = props;
 
   function handleSubmit(e: React.FormEvent) {
-    console.log("a");
     e.preventDefault();
     onGetRecipeData(categoryName);
   }
 
   return (
-    <div className="p-4 bg-blue-100 shadow-lg">
-      <h1 className="font-bold text-center text-2xl mb-4"></h1>
-      <form onSubmit={handleSubmit} className="flex flex-row gap-2 h-8 justify-end">
+    <div className="p-4 bg-blue-100 shadow-lg relative flex items-center z-10">
+      <h1 className="font-bold text-2xl absolute left-1/2 transform -translate-x-1/2">
+        レシピ検索アプリ
+      </h1>
+      <form onSubmit={handleSubmit} className="ml-auto flex flex-row gap-2 h-8">
         <input 
           type="text" 
-          placeholder="input category name" 
+          placeholder="キーワード入力" 
           value={categoryName}
           onChange={(e) => setCategoryName(e.target.value)}
           className="border p-2 rounded-md 
@@ -35,7 +36,6 @@ export default function NavigationbarComponent(props: NavigationProps) {
           検索
         </button>
       </form>
-      
     </div>
   );
 }
